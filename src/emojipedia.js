c2cc62ad535775d1d1382e21b0c9e528dba8340d
rparakh2004 <115579627+rparakh2004@users.noreply.github.com>
2023-12-21 14:35:17 +0530

    first commit

---
+++ src/emojipedia.js
@@ -0,0 +1,125 @@
+const emojipedia = [
+    {
+      id: 1,
+      emoji: "😄",
+      name: "Smiling Face with Open Mouth",
+      meaning: "Happy, cheerful, or amused.",
+    },
+    {
+      id: 2,
+      emoji: "🥺",
+      name: "Pleading Face",
+      meaning: "Begging, pleading, or urging someone to do something.",
+    },
+    {
+      id: 3,
+      emoji: "🤯",
+      name: "Exploding Head",
+      meaning: "Mind blown, shocked, or amazed.",
+    },
+    {
+      id: 4,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning: "Falling in love, feeling loved, or expressing adoration.",
+    },
+    {
+      id: 5,
+      emoji: "🤪",
+      name: "Zany Face",
+      meaning: "Goofy, silly, or joking around.",
+    },
+    {
+      id: 6,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning: "Thinking, pondering, or contemplating something.",
+    },
+    {
+      id: 7,
+      emoji: "🤫",
+      name: "Shushing Face",
+      meaning: "Telling someone to be quiet or to keep a secret.",
+    },
+    {
+      id: 8,
+      emoji: "🤨",
+      name: "Raised Eyebrow",
+      meaning: "Skeptical, suspicious, or questioning something.",
+    },
+    {
+      id: 9,
+      emoji: "🥳",
+      name: "Partying Face",
+      meaning: "Celebrating, having fun, or being in a festive mood.",
+    },
+    {
+      id: 10,
+      emoji: "🤩",
+      name: "Star-Struck",
+      meaning: "Amazed, impressed, or in awe of something.",
+    },
+    {
+      id: 11,
+      emoji: "🤯",
+      name: "Exploding Head",
+      meaning: "Mind blown, shocked, or amazed.",
+    },
+    {
+      id: 12,
+      emoji: "😎",
+      name: "Smiling Face with Sunglasses",
+      meaning: "Cool, confident, or laid-back.",
+    },
+    {
+      id: 13,
+      emoji: "🥺",
+      name: "Pleading Face",
+      meaning: "Begging, pleading, or urging someone to do something.",
+    },
+    {
+      id: 14,
+      emoji: "🤨",
+      name: "Raised Eyebrow",
+      meaning: "Skeptical, suspicious, or questioning something.",
+    },
+    {
+      id: 15,
+      emoji: "🤪",
+      name: "Zany Face",
+      meaning: "Goofy, silly, or joking around.",
+    },
+    {
+      id: 16,
+      emoji: "🥳",
+      name: "Partying Face",
+      meaning: "Celebrating, having fun, or being in a festive mood.",
+    },
+    {
+      id: 17,
+      emoji: "🤩",
+      name: "Star-Struck",
+      meaning: "Amazed, impressed, or in awe of something.",
+    },
+    {
+      id: 18,
+      emoji: "🤫",
+      name: "Shushing Face",
+      meaning: "Telling someone to be quiet or to keep a secret.",
+    },
+    {
+      id: 19,
+      emoji: "🥰",
+      name: "Smiling Face with Hearts",
+      meaning: "Falling in love, feeling loved, or expressing adoration.",
+    },
+    {
+      id: 20,
+      emoji: "🤔",
+      name: "Thinking Face",
+      meaning: "Thinking, pondering, or contemplating something.",
+    },
+  ];
+  
+  export default emojipedia;
+  
